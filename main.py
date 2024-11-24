a8aa757d545303f80c97e6ad4412849547078a3e
Haldate <603539099@qq.com>
2024-11-24 15:35:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from musk_algorithm import *
 if __name__ == '__main__':
     # 基础MUSK参数
     # 入流序列
-    input_stream = [100, 200, 300, 400, 500,
-                    500, 400, 300, 200, 100]
+    input_stream = [22, 23, 35, 71, 103,
+                    111, 109, 100, 86, 71,
+                    59, 47, 39, 32, 28,
+                    24, 22, 21, 20, 19,
+                    19, 18]
     # 入流步长
-    delta_t = 1
+    delta_t = 6
     # 蓄量系数
-    K = 3
+    K = 0.0013
     # 流量比重系数
-    X = 0.1
+    X = 0.321
 
     # 基础MUSK
     basic_output_stream, basic_S_process = basic_musk(input_stream, K, X, delta_t)
@@ -25,25 +28,34 @@ if __name__ == '__main__':
     print("Nonlinear S Process: {}".format(nonlinear_S_process))
 
     # 变系数MUSK
-    K_values = [3, 3, 3, 3, 3,
-                3, 3, 3, 3, 3]
-    X_values = [0.1, 0.1, 0.1, 0.1, 0.1,
-                0.1, 0.1, 0.1, 0.1, 0.1]
+    K_values = [0.0013, 0.0013, 0.0013, 0.0013, 0.0013,
+                0.0013, 0.0013, 0.0013, 0.0013, 0.0013,
+                0.0013, 0.0013, 0.0013, 0.0013, 0.0013,
+                0.0013, 0.0013, 0.0013, 0.0013, 0.0013,
+                0.0013, 0.0013]
+    X_values = [0.321, 0.321, 0.321, 0.321, 0.321,
+                0.321, 0.321, 0.321, 0.321, 0.321,
+                0.321, 0.321, 0.321, 0.321, 0.321,
+                0.321, 0.321, 0.321, 0.321, 0.321,
+                0.321, 0.321]
     variable_output_stream, variable_S_process = variable_musk(input_stream, K_values, X_values, delta_t)
     print("Variable Output: {}".format(variable_output_stream))
     print("Variable S Process: {}".format(variable_S_process))
 
     # 变指数非线性MUSK
     n_values = [2, 2, 2, 2, 2,
-                2, 2, 2, 2, 2]
+                2, 2, 2, 2, 2,
+                2, 2, 2, 2, 2,
+                2, 2, 2, 2, 2,
+                2, 2]
     variable_nonlinear_output_stream, variable_nonlinear_S_process = variable_nonlinear_musk(input_stream, n_values, K,
                                                                                              X, delta_t)
 
     # 变参数非线性MUSK
-    A = 0.1
-    B = 0.4
-    C = 980
-    D = 4.3
+    A = 0.103
+    B = 0.451
+    C = 982.4
+    D = 4.36
     nonlinear_with_continues_variable_output_stream, nonlinear_with_continues_variable_S_process = nonlinear_with_continues_variable_musk(
         input_stream, K, X,
         A, B, C, D,
